c982ed4e0f59471e52aae2635eb3e339c92b8293
Gaurav Sharma <gshaamzn@amazon.com>
2023-08-11 00:23:54 -0700

    Initial commit

--- lib/scheduler-lambda-cdk-stack.ts
+++ lib/scheduler-lambda-cdk-stack.ts
@@ -1,19 +1,32 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 
 export class SchedulerLambdaCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'SchedulerLambdaCdkQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    const accountInfoTable = new dynamodb.Table(this, 'AccountInfo', {
+      partitionKey: {name: 'accountId', type: dynamodb.AttributeType.STRING}
     });
 
-    const topic = new sns.Topic(this, 'SchedulerLambdaCdkTopic');
+    const mainHandler = new lambda.Function(this, 'MainHandler', {
+      runtime: lambda.Runtime.JAVA_11,
+      code: lambda.Code.fromAsset('/Users/gshaamzn/Documents/work/scheduler-lambda/app/build/libs/app-uber.jar'),
+      handler: 'scheduler.lambda.App::handleRequest',
+      timeout: Duration.minutes(5),
+      memorySize: 1024,
+      environment: {
+        ACCOUNT_INFO_TABLE_NAME: accountInfoTable.tableName
+      }
+    });
+    accountInfoTable.grantReadWriteData(mainHandler);
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    new apigw.LambdaRestApi(this, 'scheduler-endpoint', {
+      handler: mainHandler
+    })
+
   }
 }
